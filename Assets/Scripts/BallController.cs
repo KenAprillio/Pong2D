0ddcb40c70959ee65564fb633874d5e995dc77f6
KenAprillio <josiahken@gmail.com>
2021-09-03 18:50:28 +0700

    Menu akhir udah

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
     public int force;
     Rigidbody2D rigid;
+    int scoreP1, scoreP2;
+    Text scoreUIP1, scoreUIP2, txPemenang;
+    GameObject panelSelesai;
     // Start is called before the first frame update
     void Start()
     {
+        scoreP1 = 0;
+        scoreP2 = 0;
+
+        scoreUIP1 = GameObject.Find("Score1").GetComponent<Text>();
+        scoreUIP2 = GameObject.Find("Score2").GetComponent<Text>();
+
         rigid = GetComponent<Rigidbody2D>();
         Vector2 arah = new Vector2(2, 0).normalized;
         rigid.AddForce(arah * force);
+
+        panelSelesai = GameObject.Find("PanelSelesai");
+        panelSelesai.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,5 +42,50 @@ public class BallController : MonoBehaviour
             rigid.velocity = new Vector2(0, 0);
             rigid.AddForce(arah * force * 2);
         }
+
+        if (collision.gameObject.name == "TepiKanan")
+        {
+            scoreP1++;
+            ResetBall();
+            Vector2 arah = new Vector2(2, 0).normalized;
+            rigid.AddForce(arah * force);
+            if (scoreP1 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
+                txPemenang.text = "Player Biru Pemenang!";
+                Destroy(gameObject);
+                return;
+            }
+        }
+        if (collision.gameObject.name == "TepiKiri")
+        {
+            scoreP2++;
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);
+            if (scoreP2 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
+                txPemenang.text = "Player Merah Pemenang!";
+                Destroy(gameObject);
+                return;
+            }
+        }
+    }
+
+    void TampilkanScore()
+    {
+        Debug.Log("Score P1: " + scoreP1 + ", Score P2: " + scoreP2);
+        scoreUIP1.text = scoreP1 + "";
+        scoreUIP2.text = scoreP2 + "";
+    }
+
+    void ResetBall()
+    {
+        transform.localPosition = new Vector2(0, 0);
+        rigid.velocity = new Vector2(0, 0);
+        TampilkanScore();
     }
 }
